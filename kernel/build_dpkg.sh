bda8098a5394799e50c243239f2c615bb3cd2e49
Gordon Maclean <maclean@ucar.edu>
2016-01-26 10:24:12 -0700

    Add options for verbosity and distclean

--- kernel/build_dpkg.sh
+++ kernel/build_dpkg.sh
@@ -1,26 +1,66 @@
 #!/bin/bash
 
 if [ $# -lt 1 ]; then
-    echo "Usage: ${0##*/} config_file"
+    echo "Usage: ${0##*/} [-c] [-v N] config_file"
+    echo "-c: do distclean before build"
+    echo "-v N: verbosity, default=0"
     exit 1
 fi
 
-config=$1
+verbosity=0
+distclean=false
+while [ $# -gt 0 ]; do
+    case $1 in
+    -c)
+        distclean=true
+        ;;
+    -v)
+        shift
+        verbosity=$1
+        ;;
+    *)
+        config=$1
+        ;;
+    esac
+    shift
+done
+
+# With CONFIG_LOCALVERSION=titan2
+
+# No make LOCALVERSION option specified
+# CONFIG_LOCALVERSION_AUTO=y
+#   linux-image-3.16.0-titan2-00001-g1a10d22_1.0+0_armel.deb
+#   The linux-headers package then places headers in
+#   /usr/src/linux-headers-3.16.0-titan2-00001-g1a10d22/
+#   which is too hard to keep track of.
+
+# CONFIG_LOCALVERSION_AUTO is not set
+# No make LOCALVERSION option specified
+# repo dirty, results in "+" sign
+#   linux-image-3.16.0-titan2+_1.0+0_armel.deb
+# repo dirty, make LOCALVERSION="test"
+#   linux-image-3.16.0-titan2test_1.0+0_armel.deb
+# make LOCALVERSION=
+# CONFIG_LOCALVERSION_AUTO is not set
+#   linux-image-3.16.0-titan2_1.0+0_armel.deb
 
 # version number of this repository
 debver=$(git describe --match '[vV][0-9]*' 2>/dev/null || echo v1.0-0)
 debver=${debver/#v/}    # remove leading v
 debver=${debver//-/+}   # replace dashes with .
 
-cp $config linux-stable-armel/.config || exit 1
 
 cd linux-stable-armel || exit 1
 
-# make ARCH=arm distclean || exit 1
+$distclean && ( make ARCH=arm distclean || exit 1 )
 
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- oldconfig || exit 1
+lv=
+
+cp ../$config .config || exit 1
+
+make ARCH=arm oldconfig || exit 1
 
 export KBUILD_PKG_ROOTCMD='fakeroot -u'
 
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- KDEB_PKGVERSION=$debver V=1 deb-pkg || exit 1
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- LOCALVERSION=$lv KDEB_PKGVERSION=$debver V=$verbosity deb-pkg || exit 1
 
