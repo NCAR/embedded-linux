2bece660618f686113eff59f1ba436d4e6667876
Gordon Maclean <maclean@ucar.edu>
2016-01-25 18:05:17 -0700

    Simple script for editing a kernel config

---
+++ kernel/update_config.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# simple script to run menuconfig on a config file
+
+if [ $# -lt 1 ]; then
+    echo "Usage ${0##*/} config
+    exit 1
+fi
+
+config=$1
+
+cp $config linux-stable-armel/.config
+
+cd linux-stable-armel
+
+make ARCH=arm menuconfig
+
+newconfig=${config}.new
+cp .config ../$newconfig
+
+echo "config saved as $newconfig"
