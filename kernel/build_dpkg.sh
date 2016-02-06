b282552782abf464bb9fd82432f4bef5c82edf4d
Gordon Maclean <maclean@ucar.edu>
2016-02-06 16:38:11 -0700

    Updates to build scripts

--- kernel/build_dpkg.sh
+++ kernel/build_dpkg.sh
@@ -1,18 +1,69 @@
 #!/bin/bash
 
-if [ $# -lt 1 ]; then
-    echo "Usage: ${0##*/} [-c] [-v N] config_file"
-    echo "-c: do distclean before build"
+# Build Debian kernel packages for armel systems.
+
+# The plan is to have titan1 and titan2 kernels on the DSMs.
+#   /boot/vmlinuz-3.16.0-titan1
+#   /boot/vmlinuz-3.16.0-titan2
+
+# RedBoot on the DSMs will be configured to run the titan2 kernels,
+# and new kernels that are build should be titan2 kernels.
+
+# The titan1 kernel should not be updated. It is a fallback in case
+# a new titan2 kernel won't boot.
+
+# So generally CONFIG_LOCALVERSION=titan2.
+
+# Info on the interplay of CONFIG_LOCALVERSION,
+#   CONFIG_LOCALVERSION_AUTO and the LOCALVERSION make argument
+
+# make option LOCALVERSION not specified
+# CONFIG_LOCALVERSION_AUTO=y
+#   linux-image-3.16.0-titan2-00001-g1a10d22_1.0+0_armel.deb
+#   The linux-headers package then places headers in
+#   /usr/src/linux-headers-3.16.0-titan2-00001-g1a10d22/
+#   Seems too hard to keep track of.
+
+# make option LOCALVERSION not specified
+# CONFIG_LOCALVERSION_AUTO is not set
+# repo dirty, results in "+" sign
+#   linux-image-3.16.0-titan2+_1.0+0_armel.deb
+
+# repo dirty, make LOCALVERSION="test"
+#   linux-image-3.16.0-titan2test_1.0+0_armel.deb
+
+# make option blank: make LOCALVERSION=
+# CONFIG_LOCALVERSION_AUTO is not set
+#   linux-image-3.16.0-titan2_1.0+0_armel.deb
+# We'll go with this settings.
+
+set -e
+
+usage() {
+    echo "Usage: ${1##*/} [-c] [-s] [-i repository ] [-v N] config_file"
+    echo "-n: don't do a distclean before build"
+    echo "-i: install them with reprepro to the repository"
+    echo "-s: sign the package files with $key"
     echo "-v N: verbosity, default=0"
     exit 1
-fi
+}
 
+key='<eol-prog@eol.ucar.edu>'
+debarch=armel
 verbosity=0
-distclean=false
+distclean=true
+sign=false
 while [ $# -gt 0 ]; do
     case $1 in
-    -c)
-        distclean=true
+    -s)
+        sign=true
+        ;;
+    -i)
+        shift
+        repo=$1
+        ;;
+    -n)
+        distclean=false
         ;;
     -v)
         shift
@@ -25,42 +76,61 @@ while [ $# -gt 0 ]; do
     shift
 done
 
-# With CONFIG_LOCALVERSION=titan2
+[ $config ] || usage $0
 
-# No make LOCALVERSION option specified
-# CONFIG_LOCALVERSION_AUTO=y
-#   linux-image-3.16.0-titan2-00001-g1a10d22_1.0+0_armel.deb
-#   The linux-headers package then places headers in
-#   /usr/src/linux-headers-3.16.0-titan2-00001-g1a10d22/
-#   which is too hard to keep track of.
-
-# CONFIG_LOCALVERSION_AUTO is not set
-# No make LOCALVERSION option specified
-# repo dirty, results in "+" sign
-#   linux-image-3.16.0-titan2+_1.0+0_armel.deb
-# repo dirty, make LOCALVERSION="test"
-#   linux-image-3.16.0-titan2test_1.0+0_armel.deb
-# make LOCALVERSION=
-# CONFIG_LOCALVERSION_AUTO is not set
-#   linux-image-3.16.0-titan2_1.0+0_armel.deb
+localv=
 
 # version number of this repository
 debver=$(git describe --match '[vV][0-9]*' 2>/dev/null || echo v1.0-0)
 debver=${debver/#v/}    # remove leading v
 debver=${debver//-/+}   # replace dashes with .
 
+cd linux-stable-armel
 
-cd linux-stable-armel || exit 1
+$distclean && make ARCH=arm distclean
 
-$distclean && ( make ARCH=arm distclean || exit 1 )
+cp ../$config .config
 
-lv=
-
-cp ../$config .config || exit 1
-
-make ARCH=arm oldconfig || exit 1
+make ARCH=arm oldconfig
 
 export KBUILD_PKG_ROOTCMD='fakeroot -u'
 
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- LOCALVERSION=$lv KDEB_PKGVERSION=$debver V=$verbosity deb-pkg || exit 1
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- LOCALVERSION=$localv KDEB_PKGVERSION=$debver V=$verbosity deb-pkg
+
+kver=$(make kernelversion)  # 3.16.0
+krel=$(make ARCH=arm kernelrelease)  # 3.16.0-titanN+
+
+cd ..
+
+pkgs=$(echo linux-firmware-image-${kver}-*_${debarch}.deb \
+    linux-headers-${kver}-*_${debarch}.deb \
+    linux-image-${kver}-*_${debarch}.deb)
+
+if $sign; then
+    for pkg in $pkgs; do
+        dpkg-sig --sign builder -k "$key" $pkg
+    done
+fi
+
+# remove _debver_armel.deb from pkg file names
+pkgnames=
+for pkg in $pkgs; do
+    pkg=${pkg%_*}
+    pkg=${pkg%_*}
+    pkgnames="$pkgnames $pkg"
+done
+
+if [ -n "$repo" ]; then
+    umask 0002
+    set -e
+    flock $repo sh -c "
+        reprepro -V -b $repo remove jessie $pkgnames;
+        reprepro -V -b $repo deleteunreferenced;
+        reprepro -V -b $repo includedeb jessie $pkgs"
+
+    rm -f $pkgs linux-libc-dev*${debarch}.deb
+
+else
+    echo "packages: $pkgs"
+fi
 
--- kernel/jenkins_build.sh
+++ kernel/jenkins_build.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
+if [ $# -lt 1 ]; then
+    echo "$0 repository"
+    exit 1
+fi
+
+# repo=/net/ftp/pub/temp/users/maclean/debian
+repo=$1
+
 # Build kernel for a config file if the md5sum of
 # the config file has changed.
 
 # If you just change a simple thing like adding a kernel module
 # to the config and are confident that it won't break things on the
-# target system (i.e. are brave and foolish) then you can
-# make that change without changing CONFIG_LOCALVERSION in
-# the file.
+# target system then you can make that change without changing
+# CONFIG_LOCALVERSION in the config file.
 
 # If you have made significant changes and you don't want the
 # new kernel to overwrite the old on the target system, then
@@ -28,10 +35,7 @@ save_md5() {
 # for file in config-3.16-titan config-3.16-viper; do
 for file in config-3.16-titan; do
     if ! check_md5 $file > /dev/null; then
-        ./build_dpkg.sh $file && save_md5 $file
-        # todo:
-        #   sign .deb files
-        #   copy to debian repo
+        ./build_dpkg.sh -s -i $repo $file && save_md5 $file
     else
         echo "No changes to $file since last build"
     fi
