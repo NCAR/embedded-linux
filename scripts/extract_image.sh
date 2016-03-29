d80c1706c69bb7dc80142ce951da8a11f9ae8768
Gordon Maclean <maclean@ucar.edu>
2016-03-28 21:57:54 -0600

    Script to extract image from flash disk

---
+++ scripts/extract_image.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+if [ $# -lt 2 ]; then
+    echo "Usage ${0##*/} device imagefile"
+    exit 1
+fi
+
+dev=$1
+dest=$2
+
+set -e
+
+doxz=false
+[ ${dest##*.} == xz ] && doxz=true
+
+$doxz && dest=${dest%.*}
+
+tmpfile=$(mktemp /tmp/${0##*/}_XXXXXX.img)
+tmpfile2=$(mktemp /tmp/${0##*/}_XXXXXX.img)
+bmapfile=$(mktemp /tmp/${0##*/}_XXXXXX.bmap)
+trap "{ rm -f $tmpfile $tmpfile2 $bmapfile; }" EXIT
+
+sudo dd if=$dev of=$tmpfile bs=4M
+
+cp --sparse=always $tmpfile $tmpfile2
+
+bmaptool create -o $bmapfile $tmpfile2
+
+bmaptool copy --bmap $bmapfile $tmpfile2 $dest
+
+$doxz && xz $dest
+
+cp $bmapfile ${dest%.*}.bmap
+
