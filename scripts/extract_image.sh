4e7c019bd296cf6a78d5ff9a114174bc93ddb15b
Gordon Maclean <maclean@ucar.edu>
2016-07-19 14:04:41 -0600

    Set size of target image

--- scripts/extract_image.sh
+++ scripts/extract_image.sh
@@ -7,7 +7,7 @@ if [ $# -lt 2 ]; then
     exit 1
 fi
 
-dev=$1
+fdev=$1
 dest=$2
 
 set -e
@@ -19,11 +19,11 @@ doxz=false
 
 $doxz && dest=${dest%.*}
 
-fs="$(mount | grep "^$dev" | awk '{print $3}')"
+fs="$(mount | grep "^$fdev" | awk '{print $3}')"
 
 if [ -n "$fs" ]; then
-    echo "Umounting file systems on $dev"
-    umount $(mount | grep "^$dev" | awk '{print $3}')
+    echo "Umounting file systems on $fdev"
+    umount $(mount | grep "^$fdev" | awk '{print $3}')
 fi
 
 tmpfile=$(mktemp --tmpdir ${0##*/}_XXXXXX.img)
@@ -31,15 +31,35 @@ tmpfile2=$(mktemp --tmpdir ${0##*/}_XXXXXX.img)
 bmapfile=$(mktemp --tmpdir ${0##*/}_XXXXXX.bmap)
 trap "{ rm -f $tmpfile $tmpfile2 $bmapfile; }" EXIT
 
-sudo dd if=$dev of=$tmpfile bs=4M
+# Determine last sector of partitions, only extract up to that.
+# Image should then fit on all disks of at least that size
+sudo fdisk -l $fdev > $tmpfile || exit 1
+lastsect=$(awk 'ENDFILE{print $3}' $tmpfile)
 
+blocksize=512
+count=$lastsect
+for fact in 5 10 20 50 100 200 500; do
+    [ $(( $lastsect % $fact )) -ne 0 ] && break
+    blocksize=$(( 512 * $fact ))
+    count=$(( $lastsect / $fact ))
+done
+
+echo "Extracting image, #sectors=$lastsect, blocksize=$blocksize, count=$count"
+sudo dd if=$fdev of=$tmpfile bs=$blocksize count=$count
+
+echo "Creating sparse image file"
 cp --sparse=always $tmpfile $tmpfile2
 
+echo "Creating bmap file"
 bmaptool create -o $bmapfile $tmpfile2
 
-echo "Compressing. This will take some time..."
 
-$doxz && xz -c $tmpfile2 > $dest.xz
+if $doxz; then
+    echo "Compressing. This will take some time..."
+    xz -c $tmpfile2 > $dest.xz
+else
+    mv $tmpfile2 $dest
+fi
 
 cp $bmapfile ${dest%.*}.bmap
 chmod +rw ${dest%.*}.bmap
--- scripts/rpi_partition.sh
+++ scripts/rpi_partition.sh
@@ -9,18 +9,24 @@ fi
 fdev=$1
 sizep2=$2
 
-# Leave some space at the end of the disk, so that
-# the image can be copied to slightly smaller SDs.
-# one sector=512 bytes
-unused_sectors=20
+# How big to make the image. one sector=512 bytes
+# 8GB Kingston: 15564800 sectors
+# 8GB Sandisk:  15523840 sectors (40960 sectors smaller)
+target_size=15523840
 
 tmpfile=$(mktemp)
 trap "{ rm -f $tmpfile; }" EXIT
 
 fdisk -l $fdev > $tmpfile || exit 1
 
-totsize=$(sed -n -r -e "/^Disk \/dev\/${fdev##*/}:/s/.* ([0-9]+) sectors/\1/p" $tmpfile)
-# echo "totsize=$totsize"
+disk_size=$(sed -n -r -e "/^Disk \/dev\/${fdev##*/}:/s/.* ([0-9]+) sectors/\1/p" $tmpfile)
+echo "target_size=$target_size sectors, size of this disk=$disk_size"
+
+if [ $disk_size -lt $target_size ]; then
+    echo "Disk is $disk_size sectors, smaller than target_size=$target_size"
+    exit 1
+fi
+
 startp2=$(grep ${fdev}p2  $tmpfile | awk '{print $2}')
 
 # delete partitions 2 and 3, create new partion 2 of requested size
@@ -39,7 +45,7 @@ EOD
 
 endp2=$(fdisk -l $fdev | grep ${fdev}p2 | awk '{print $3}')
 startp3=$(( $endp2 + 1 ))
-sizep3=$(( $totsize - $startp3 - $unused_sectors ))
+sizep3=$(( $target_size - $startp3 ))
 
 fdisk $fdev > /dev/null << EOD
 n
@@ -54,6 +60,4 @@ fdisk -l $fdev
 
 resize2fs ${fdev}p2
 mkfs.ext4 -F ${fdev}p3
-
-
 
