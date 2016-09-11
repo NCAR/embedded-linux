2d11b7831499ecb4d841719cc71c564822c5be6b
Gordon Maclean <maclean@ucar.edu>
2016-09-11 10:58:46 -0600

    ptpv1d supports version 1 of IEEE 1588 PTP

---
+++ software/ptpv1d/build_dpkg.sh
@@ -0,0 +1,88 @@
+#!/bin/sh
+
+pkg=ptpv1d
+
+key="<eol-prog@eol.ucar.edu>"
+
+usage() {
+    echo "Usage: ${1##*/} [-s] [-i repository ] arch"
+    echo "-s: sign the package files with key=$key"
+    echo "-i: install them with reprepro to the repository"
+    echo "arch is armel"
+    exit 1
+}
+
+if [ $# -lt 1 ]; then
+    usage $0
+fi
+
+sign=false
+arch=armel
+while [ $# -gt 0 ]; do
+    case $1 in
+    -s)
+        sign=true
+        ;;
+    -i)
+        shift
+        repo=$1
+        ;;
+    armel)
+        export CC=arm-linux-gnueabi-gcc
+        arch=$1
+        ;;
+    *)
+        usage $0
+        ;;
+    esac
+    shift
+done
+
+script=${0##*/}
+
+sdir=$(dirname $0)
+cd $sdir
+sdir=$PWD
+
+args="-a$arch"
+karg=
+if $sign; then
+    export GPG_AGENT_INFO
+    if [ -e $HOME/.gpg-agent-info ]; then
+        . $HOME/.gpg-agent-info
+    else
+        echo "Warning: $HOME/.gpg-agent-info not found"
+    fi
+    karg=-k"$key"
+else
+    args="$args -us -uc"
+fi
+
+rm -f ${pkg}_*_$arch.changes
+
+rm -rf ${pkg}-1.0
+
+tar xJf ${pkg}_1.0.orig.tar.xz
+cp -r debian ${pkg}-1.0
+
+cd ${pkg}-1.0
+
+debuild $args "$karg"
+
+cd ..
+
+changes=${pkg}_*_armel.changes
+
+pkgs=$(grep "^Binary:" $changes | sed -e s/Binary://)
+archs=$(grep "^Architecture:" $changes | sed -e 's/Architecture: *//' | tr \  "|")
+
+if [ -n "$repo" ]; then
+    umask 0002
+        
+    flock $repo sh -c "
+        reprepro -V -b $repo -A '$archs' remove jessie $pkgs;
+        reprepro -b $repo deleteunreferenced;
+        reprepro -V -b $repo -A '$archs' include jessie $changes"
+else
+    echo "Results in $sdir"
+fi
---
+++ software/ptpv1d/jenkins_build.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+
+if [ $# -lt 1 ]; then
+    echo "$0 repository"
+    exit 1
+fi
+
+# repo=/net/ftp/pub/temp/users/maclean/debian
+repo=$1
+
+tmpdir=$(mktemp -d /tmp/${0##*/}_XXXXXX)
+trap "{ rm -rf $tmpdir; }" EXIT
+
+check_md5() {
+    local file=$1
+    local sumfile=.${file}_md5sum
+    md5sum --quiet --check $sumfile 2>/dev/null
+    return $?
+}
+save_md5() {
+    local file=$1
+    local sumfile=.${file}_md5sum
+    md5sum $file > $sumfile
+}
+
+check_md5_dir() {
+    local dir=$1
+    local sumfile=.${dir}_md5sum
+    tar cf $tmpdir/${dir}.tar --mtime="1970-1-1" $dir
+    [ -f $sumfile ] && cp $sumfile $tmpdir
+    cd $tmpdir
+    md5sum --quiet --check $sumfile 2>/dev/null
+    ok=$?
+    cd -
+    return $ok
+}
+
+save_md5_dir() {
+    local dir=$1
+    local sumfile=.${dir}_md5sum
+    tar cf $tmpdir/${dir}.tar --mtime="1970-1-1" $dir
+    cd $tmpdir
+    md5sum ${dir}.tar > $sumfile
+    cd -
+    mv $tmpdir/$sumfile .
+}
+
+export GPG_AGENT_INFO
+[ -e $HOME/.gpg-agent-info ] && . $HOME/.gpg-agent-info
+
+dirs="ptpv1d-1.0"
+files="build_dpkg.sh jenkins_build.sh ptpv1d_1.0.orig.tar.gz"
+
+changed=false
+for dir in $dirs; do
+    check_md5_dir $dir || changed=true
+done
+for file in $files; do
+    check_md5 $file > /dev/null || changed=true
+done
+
+if ! $changed; then
+    echo "No changes since last build"
+    exit
+fi
+
+./build_dpkg.sh -s -i $repo armel || exit 1
+
+for dir in $dirs; do
+    save_md5_dir $dir
+done
+for file in $files; do
+    save_md5 $file
+done
+
