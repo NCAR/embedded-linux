2c0d4949a7570fe8e5ed15469b5074312511768f
Gordon Maclean <maclean@ucar.edu>
2016-09-12 09:14:45 -0600

    More fixes to build, postinst,postrm files

--- software/ptpv1d/build_dpkg.sh
+++ software/ptpv1d/build_dpkg.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash -e
 
 pkg=ptpv1d
 
@@ -7,8 +7,9 @@ key="<eol-prog@eol.ucar.edu>"
 usage() {
     echo "Usage: ${1##*/} [-s] [-i repository ] arch"
     echo "-s: sign the package files with key=$key"
+    echo "-c: build in a chroot"
     echo "-i: install them with reprepro to the repository"
-    echo "arch is armel"
+    echo "arch is armel or armhf"
     exit 1
 }
 
@@ -17,9 +18,13 @@ if [ $# -lt 1 ]; then
 fi
 
 sign=false
+use_chroot=false
 arch=armel
 while [ $# -gt 0 ]; do
     case $1 in
+    -c)
+        use_chroot=true
+        ;;
     -s)
         sign=true
         ;;
@@ -42,13 +47,25 @@ while [ $# -gt 0 ]; do
     shift
 done
 
-script=${0##*/}
+if $use_chroot; then
+    dist=$(lsb_release -c | awk '{print $2}')
+    if [ $arch == amd64 ]; then
+        chr_name=${dist}-amd64-sbuild
+    else
+        chr_name=${dist}-amd64-cross-${arch}-sbuild
+    fi
+    if ! schroot -l | grep -F chroot:${chr_name}; then
+        echo "chroot named ${chr_name} not found"
+        exit 1
+    fi
+fi
 
 sdir=$(dirname $0)
 cd $sdir
 sdir=$PWD
 
 args="-a$arch"
+
 karg=
 if $sign; then
     export GPG_AGENT_INFO
@@ -71,22 +88,40 @@ tar xJf ${pkg}_1.0.orig.tar.xz
 
 cd ${pkg}-1.0
 
-debuild $args "$karg"
+if $use_chroot; then
+    echo "Starting schroot, which takes some time ..."
+    schroot -c $chr_name --directory=$PWD << EOD
+        set -e
+        [ -f $HOME/.gpg-agent-info ] && . $HOME/.gpg-agent-info
+        export GPG_AGENT_INFO
+        debuild $args "$karg"
+EOD
+else
+    debuild $args "$karg"
+fi
 
 cd ..
 
-changes=${pkg}_*_armel.changes
-
-pkgs=$(grep "^Binary:" $changes | sed -e s/Binary://)
-archs=$(grep "^Architecture:" $changes | sed -e 's/Architecture: *//' | tr \  "|")
+set -x
 
 if [ -n "$repo" ]; then
     umask 0002
-        
+
+    echo "Build results:"
+    ls
+    echo ""
+
+    changes=${pkg}_*_${arch}.changes
+    echo "Changes file: $changes"
+    cat $changes
+    echo ""
+
     flock $repo sh -c "
-        reprepro -V -b $repo -A '$archs' remove jessie $pkgs;
-        reprepro -b $repo deleteunreferenced;
-        reprepro -V -b $repo -A '$archs' include jessie $changes"
+        reprepro -V -b $repo -C main include jessie $changes;
+        reprepro -b $repo deleteunreferenced"
+
+    rm -f ${pkg}_*_$arch.build ${pkg}_*.dsc ${pkg}_*.tar.xz ${pkg}*_$arch.deb $changes
+
 else
     echo "Results in $sdir"
 fi
